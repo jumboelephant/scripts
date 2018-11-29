502580ffe4ebb685c784c5a67670940327f4491a
David Mitchell <davidgordonmitchell@yahoo.co.uk>
2018-11-29 00:00:41 +0000

    commit

--- provider.tf
+++ provider.tf
@@ -1,13 +1,11 @@
 provider "google"
 {
-
   credentials = "${var.gcloudPassword}"
   project = ""
   region = ""
-  
 }
-
-resource "google_compute_instance" "ubuntuDC" {
+resource "google_compute_instance" "ubuntuDC"
+{
   "boot_disk"
   {
     device_name = "sda"
@@ -15,4 +13,21 @@ resource "google_compute_instance" "ubuntuDC" {
   machine_type = "ubuntu-"
   name = ""
   "network_interface" {}
+}
+data "google_compute_network" "Corp"
+{
+  name = "Corp"
+  project = ""
+}
+data "google_compute_subnetwork" "CorpSubnet"
+{
+  name = "CorpSubnet"
+  network = "1st"
+  ip_cidr_range = "10.10.10.0/24"
+  region = ""
+  depends_on = ["data.google_compute_network.Corp"]
+}
+output "String"
+{
+  value = "Built some shit right here dudes"
 }
